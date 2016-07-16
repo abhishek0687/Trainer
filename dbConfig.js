ae2349a8be02000e4d1f373d4937ecb86935f04c
abhishek <abhishek0687@gmail.com>
2016-07-16 13:25:56 +0530

    Deploy

--- dbConfig.js
+++ dbConfig.js
@@ -12,7 +12,9 @@ var storage =   multer.diskStorage({
 
 exports.upload = multer({ storage : storage}).single('userPhoto');
 
-exports.url = 'mongodb://localhost:27017/trainers';
+exports.url ='mongodb://TECHUSER:TECHUSR@ds017175.mlab.com:17175/testandtrain';
+
+//exports.url = 'mongodb://localhost:27017/trainers';
 
 exports.webPort = 9001;
 
